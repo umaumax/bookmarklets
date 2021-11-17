b561a75a6ba0ea4d029f2a816f990a1eb653ac72
umaumax <umauma7083@gmail.com>
2021-11-18 08:38:58 +0900

    Add qiita-trend.user.js

---
+++ qiita-trend.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Filter Latest Trend at Qiita
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  filter yersterday topics only
+// @author       You
+// @match        https://qiita.com/trend
+// @icon         https://www.google.com/s2/favicons?domain=qiita.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let header = document.getElementsByClassName('st-NewHeader_navigationTabContainer')[1];
+    let a = document.createElement("a");
+    a.href = '#';
+    a.appendChild(document.createTextNode("最新"));
+    a.classList.add('st-NewHeader_subNavigationItem');
+    a.onclick = function filter_lateset() {
+        document.getElementsByClassName('st-NewHeader_subNavigationItem').forEach(function(e) {
+            e.classList.remove('is-active');
+        });
+        a.classList.add('is-active');
+
+        let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+        document.querySelectorAll('article section div > p').forEach(function(e) {
+            let match = e.innerText.match(/(?<year>[0-9]+)年(?<month>[0-9]+)月(?<day>[0-9]+)日/);
+            if (match && (match.groups.month != (yesterday.getMonth() + 1) || match.groups.day != yesterday.getDate())) {
+                e.closest('article').style.display = 'none';
+            }
+        })
+    }
+    header.appendChild(a);
+})();
