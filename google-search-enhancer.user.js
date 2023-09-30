56639b04692884831723122cab96001ebc9da4a1
umaumax <umauma7083@gmail.com>
2023-09-30 16:19:01 +0900

    Update google-search-enhancer.user.js

--- google-search-enhancer.user.js
+++ google-search-enhancer.user.js
@@ -31,13 +31,25 @@
         }
     ]
 
-    var e = document.querySelector('div[role="navigation"] div[role="navigation"]>div[data-st-tgt="mode"]>div>div>div');
-    console.log(e)
-    var base_bode = e.querySelector('a');
+    var search_bar_box_query = 'div[role="navigation"]';
+    var e = document.querySelector(search_bar_box_query);
+    if (e == null) {
+        // for telling error status to user
+        document.body.style.backgroundColor = "#faf0f0";
+        console.log("[google search feature enhancer] There is no element!:" + search_bar_box_query)
+        return
+    }
+    var base_node = e.querySelector('a');
+    if (base_node == null) {
+        // for telling error status to user
+        document.body.style.backgroundColor = "#f0faf0";
+        console.log("[google search feature enhancer] There is no element!:" + search_bar_box_query + ' a')
+        return
+    }
     inputs.forEach(v => {
-        var new_node = base_bode.cloneNode(true);
+        var new_node = base_node.cloneNode(true);
         new_node.href = location.href + v.query;
         new_node.querySelector('span').textContent = v.label;
-        e.prepend(new_node);
+        base_node.parentNode.prepend(new_node);
     });
 })();
