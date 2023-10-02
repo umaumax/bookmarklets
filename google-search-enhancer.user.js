157e147b28fa797786e91392939dc9483bcb0baf
umaumax <umauma7083@gmail.com>
2023-10-02 20:55:10 +0900

    Update google-search-enhancer.user.js

--- google-search-enhancer.user.js
+++ google-search-enhancer.user.js
@@ -10,6 +10,29 @@
 // @require      http://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
+function findCommonAncestor(elements) {
+    if (!Array.isArray(elements) || elements.length === 0) {
+        return null;
+    }
+
+    let commonAncestor = elements[0].parentNode;
+
+    for (let i = 1; i < elements.length; i++) {
+        let currentElement = elements[i];
+        while (commonAncestor) {
+            if (commonAncestor.contains(currentElement)) {
+                break;
+            }
+            commonAncestor = commonAncestor.parentNode;
+        }
+        if (!commonAncestor) {
+            return null;
+        }
+    }
+
+    return commonAncestor;
+}
+
 (function() {
     'use strict';
 
@@ -31,27 +54,20 @@
         }
     ]
 
-    const firstHitSearchButtonElements = document.querySelectorAll('span');
-    const firstHitSearchButton = Array.from(firstHitSearchButtonElements).find((element) => {
+    const base_node = findCommonAncestor(Array.from(document.querySelectorAll('div,span')).filter((element) => {
         return ['画像', '動画', 'ショッピング', 'ニュース', '地図'].includes(element.textContent);
-    });
-    if (firstHitSearchButton == null) {
+    })) ? .firstElementChild;
+    if (base_node == null) {
         // for telling error status to user
         document.body.style.backgroundColor = "#faf0f0";
         console.log("[google search feature enhancer] There is no element!")
         return
     }
-    const base_node = firstHitSearchButton.closest('a')
-    if (base_node == null) {
-        // for telling error status to user
-        document.body.style.backgroundColor = "#f0faf0";
-        console.log("[google search feature enhancer] There is no parent element!:")
-        return
-    }
     inputs.forEach(v => {
         var new_node = base_node.cloneNode(true);
-        new_node.href = location.href + v.query;
-        new_node.querySelector('span').textContent = v.label;
+        var a_node = new_node.tagName == 'A' ? new_node : new_node.querySelector('a')
+        a_node.href = location.href + v.query;
+        a_node.querySelector('span').textContent = v.label;
         base_node.parentNode.prepend(new_node);
     });
 })();
