f9413d110f08dcbe70ae0c0c1b2f4e10ba5e7703
umaumax <umauma7083@gmail.com>
2023-10-02 21:07:37 +0900

    Update google-search-enhancer

--- google-search-enhancer.user.js
+++ google-search-enhancer.user.js
@@ -10,6 +10,30 @@
 // @require      http://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
+function findDeepestChildMatchingCondition(element, conditionFunction) {
+    if (!element) {
+        return null;
+    }
+
+    let deepestChild = null;
+    let maxDepth = -1;
+
+    function traverse(node, depth) {
+        if (conditionFunction(node) && depth > maxDepth) {
+            deepestChild = node;
+            maxDepth = depth;
+        }
+
+        for (const child of node.childNodes) {
+            traverse(child, depth + 1);
+        }
+    }
+
+    traverse(element, 0);
+
+    return deepestChild;
+}
+
 function findCommonAncestor(elements) {
     if (!Array.isArray(elements) || elements.length === 0) {
         return null;
@@ -56,7 +80,7 @@ function findCommonAncestor(elements) {
 
     const base_node = findCommonAncestor(Array.from(document.querySelectorAll('div,span')).filter((element) => {
         return ['画像', '動画', 'ショッピング', 'ニュース', '地図'].includes(element.textContent);
-    })) ? .firstElementChild;
+    }))?.firstElementChild;
     if (base_node == null) {
         // for telling error status to user
         document.body.style.backgroundColor = "#faf0f0";
@@ -67,7 +91,10 @@ function findCommonAncestor(elements) {
         var new_node = base_node.cloneNode(true);
         var a_node = new_node.tagName == 'A' ? new_node : new_node.querySelector('a')
         a_node.href = location.href + v.query;
-        a_node.querySelector('span').textContent = v.label;
+        var text_node = findDeepestChildMatchingCondition(new_node, (element) => {
+            return ['画像', '動画', 'ショッピング', 'ニュース', '地図'].includes(element.textContent);
+        });
+        text_node.textContent = v.label;
         base_node.parentNode.prepend(new_node);
     });
 })();
