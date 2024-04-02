82ba74ecbd17d1151fba6d54804162bbfe67448d
umaumax <umauma7083@gmail.com>
2024-04-02 21:36:00 +0900

    Update jira-extensions.user.js

--- jira-extensions.user.js
+++ jira-extensions.user.js
@@ -24,35 +24,47 @@ String.prototype.hashCode = function() {
 }
 
 function get_hash_color(text) {
-    return "hsl(" + text.hashCode() % 360 + "%, 75%, 75%)";
+    return "hsl(" + text.hashCode() % 360 + ", 75%, 75%)";
 }
 
 (async function() {
     'use strict';
 
-    while (document.querySelectorAll('.ghx-issue-content .ghx-extra-field-content').length == 0) {
-        await sleep(100);
-    }
-
-    // hide line which displays labels
-    // backlog window
-    Array.from(document.querySelectorAll('.ghx-plan-extra-fields.ghx-plan-extra-fields-1.ghx-row')).forEach((e) => e.style.display = "none")
-    // active sprint window
-    Array.from(document.querySelectorAll('.ghx-issue-content .ghx-extra-field-row')).forEach((e) => e.style.display = "none")
-
-    // create elemnts of labels
-    Array.from(document.querySelectorAll('.ghx-issue-content')).forEach((ticket) => {
-        let extra_line = ticket.querySelector('.ghx-extra-field-content')
-        if (extra_line != null && extra_line.innerText != "None") {
-            extra_line.innerText.split(',').forEach((text) => {
-                let label = document.createElement("aui-badge")
-                label.style.padding = "6px"
-                label.style.margin = "4px"
-                label.style.color = "black"
-                label.style.background = get_hash_color(text)
-                label.textContent = text
-                ticket.querySelector('.ghx-summary').after(label)
-            })
+    while (true) {
+        while (document.querySelectorAll('.ghx-issue-content .ghx-extra-field-content').length == 0) {
+            await sleep(100);
         }
-    })
+        await sleep(100);
+
+        // hide line which displays labels
+        // backlog
+        let switched_flag = false;
+        Array.from(document.querySelectorAll('.ghx-plan-extra-fields.ghx-plan-extra-fields-1.ghx-row')).forEach((e) => {
+            switched_flag |= e.style.display != "none";
+            e.style.display = "none";
+        })
+        // active sprint
+        Array.from(document.querySelectorAll('.ghx-issue-content .ghx-extra-field-row')).forEach((e) => {
+            switched_flag |= e.style.display != "none";
+            e.style.display = "none";
+        })
+        if (!switched_flag) continue;
+
+
+        // create labels of elements
+        Array.from(document.querySelectorAll('.ghx-issue-content')).forEach((ticket) => {
+            let extra_line = ticket.querySelector('.ghx-extra-field-content')
+            if (extra_line != null && extra_line.innerText != "None") {
+                extra_line.innerText.split(',').forEach((text) => {
+                    let label = document.createElement("aui-badge")
+                    label.style.padding = "6px"
+                    label.style.margin = "4px"
+                    label.style.color = "black"
+                    label.style.background = get_hash_color(text)
+                    label.textContent = text
+                    ticket.querySelector('.ghx-summary').after(label)
+                })
+            }
+        })
+    }
 })()
