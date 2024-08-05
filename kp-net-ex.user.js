ffdb3615fa72002bf5b2996bdc625716dffee851
umaumax <umauma7083@gmail.com>
2024-08-05 23:25:48 +0900

    Update

--- kp-net-ex.user.js
+++ kp-net-ex.user.js
@@ -9,6 +9,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.kids.isas.jaxa.jp
 // ==/UserScript==
 
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
 function searchTextContents(element_tag, text) {
     return Array.from(document.querySelectorAll(element_tag))
         .filter((element) => element.textContent === text)
@@ -22,13 +24,17 @@ function searchTextContent(element_tag, text) {
     return null;
 }
 
-function check_auto_login() {
+async function check_auto_login() {
     console.log(Date(Date.now()));
-    console.log('check_auto_login')
+    console.log('[check_auto_login]')
     console.log('url', document.URL)
     console.log('referrer', document.referrer)
-    const session_timeout_url = 'https://ctrl.kp-net.com/settingcontrol/remotevisualization/simplevisualization/updating/enduser'
-    if (document.URL == session_timeout_url) {
+
+    const session_timeout_urls = [
+        'https://ctrl.kp-net.com/settingcontrol/remotevisualization/simplevisualization/updating/enduser',
+        'https://ctrl.kp-net.com/settingcontrol/processLogin',
+    ];
+    if (session_timeout_urls.includes(document.URL)) {
         if (searchTextContent('span', 'セッションタイムアウト')) {
             let button = searchTextContent('button', 'ログインに戻る');
             if (button) {
@@ -42,7 +48,11 @@ function check_auto_login() {
     }
 
     if (document.URL.match('https://ctrl.kp-net.com/settingcontrol/login.*')) {
-        if (document.referrer == session_timeout_url || document.referrer.match('https://ctrl.kp-net.com/settingcontrol/login.*')) {
+        // NOTE: This is need to refresh input forms' values.
+        document.getElementById('loginid').focus()
+        // NOTE: Wait until the focus process on the element is complete.
+        await sleep(1000)
+        if (session_timeout_urls.includes(document.referrer) || document.referrer.match('https://ctrl.kp-net.com/settingcontrol/login.*')) {
             let button = searchTextContent('button', 'ログイン');
             console.log(button)
             if (button) {
@@ -56,9 +66,12 @@ function check_auto_login() {
 
 }
 
-(function() {
+(async function() {
     'use strict';
 
     let check_auto_login_interval_ms = 60 * 1000;
-    setInterval(check_auto_login, check_auto_login_interval_ms);
+    while (true) {
+        await check_auto_login()
+        await sleep(check_auto_login_interval_ms)
+    }
 })();
