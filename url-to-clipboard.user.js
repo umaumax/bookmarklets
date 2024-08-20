ed1084daeb3e866adf51e99a20b3263c2351a440
umaumax <umauma7083@gmail.com>
2024-08-20 23:09:02 +0900

    Update

--- url-to-clipboard.user.js
+++ url-to-clipboard.user.js
@@ -153,7 +153,8 @@ function get_selection_text() {
 function url_to_clipboard() {
     var url = location.href;
     var title = get_selection_text() || document.title || url;
-    var encoded_title = title.replaceAll(/([\[\]<>])/g, '\\$1');
+    // var encoded_title = title.replaceAll(/([\[\]<>])/g, '\\$1');
+    var encoded_title = title.replaceAll('[', '(').replaceAll(']', ')').replaceAll(/([\[\]<>])/g, '\\$1');
     var content = `[${encoded_title}]( ${url} )`;
     GM_setClipboard(content);
 
