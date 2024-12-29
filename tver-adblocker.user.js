f73e6fa2bb83000f2acc8af9e5545574feb17684
umaumax <umauma7083@gmail.com>
2024-12-29 16:40:09 +0900

    Add noframes

--- tver-adblocker.user.js
+++ tver-adblocker.user.js
@@ -7,6 +7,7 @@
 // @match        https://tver.jp/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tver.jp
 // @grant        none
+// @noframes
 // ==/UserScript==
 
 function is_video_playing(video) {
